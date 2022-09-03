9c7e766547df9f85a1128d1f9d58edab943e85d5
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2022-09-03 14:49:30 +0200

    Revert "more player actions + fix capitalization issue in resolution"
    This reverts commit d2ab26d78b9b5f2f0a3fe88f88ad79bb8ca4910c.

--- src/main.rs
+++ src/main.rs
@@ -667,7 +667,8 @@ impl Card<'_>
 			{
 				text.push_str("is sent from your ");
 				Self::generate_location(text);
-				text.push_str(" to the GY");
+				text.push_str(" to your ");
+				Self::generate_location(text);
 			}
 			_ => todo!("text so far: {}", text)
 		}
@@ -798,7 +799,7 @@ impl Card<'_>
 		}
 		if locations.len() > 1
 		{
-			text.push_str(" and/or ");
+			text.push_str("and/or");
 		}
 		text.push_str(locations.last().unwrap());
 	}
@@ -990,7 +991,6 @@ impl Card<'_>
 					text.push_str("up to ");
 				}
 				text.push_str(&amount.to_string());
-				text.push(' ');
 				Self::generate_card_anywhere(text, false);
 				text.push_str(" from your ");
 				Self::generate_add_location(text);
@@ -1022,8 +1022,7 @@ impl Card<'_>
 		if rng.gen::<f32>() < PERCENTAGE_RESOLUTION_HAS_CONJUNCTION
 		{
 			Self::generate_conjunction(text);
-			text.push(' ');
-			Self::generate_resolution(text, referrer, false);
+			Self::generate_resolution(text, referrer, start_of_sentence);
 		}
 	}
 
@@ -1059,7 +1058,7 @@ impl Card<'_>
 							self.text.push_str("first ");
 						}
 						self.text.push_str("be ");
-						self.text.push_str(summoning_type);
+						Self::generate_summoning_type(&mut self.text);
 						self.text.push_str("ed by ");
 						Self::generate_imperative_action(&mut self.text);
 						Self::generate_target(&mut self.text);
